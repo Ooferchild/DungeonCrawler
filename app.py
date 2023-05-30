9d230511c15529ec45a620e8427e005807277f8d
Ooferchild <kushnirjoshua@gmail.com>
2023-05-30 11:53:08 -0400

    School 3

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 import random
-import shelve
 
 from azure.cosmos import CosmosClient
 from dotenv import load_dotenv
@@ -10,8 +9,10 @@ from flask import Flask, redirect, render_template, request, url_for
 load_dotenv()
 app = Flask(__name__)
 
+
 def get_cosmos_client():
     return CosmosClient(url=os.getenv('ENDPOINT_URI'), credential=os.getenv('PRIMARY_KEY'))
+
 
 def loot_table():
     items = [
@@ -26,34 +27,36 @@ def loot_table():
     ]
     return random.choices(items, [weight for _, weight, _ in items])[0]
 
+
 def monsters(name, health, strength, defense, dexterity):
     level1 = [
-        {name : "goblin", health : 10, strength : 5, defense : 5, dexterity : 5},
-        {name : "orc", health : 15, strength : 10, defense : 10, dexterity : 10},
-        {name : "skeleton", health : 20, strength : 15, defense : 15, dexterity : 15},
-        {name : "zombie", health : 25, strength : 20, defense : 20, dexterity : 20},
+        {name: "goblin", health: 10, strength: 5, defense: 5, dexterity: 5},
+        {name: "orc", health: 15, strength: 10, defense: 10, dexterity: 10},
+        {name: "skeleton", health: 20, strength: 15, defense: 15, dexterity: 15},
+        {name: "zombie", health: 25, strength: 20, defense: 20, dexterity: 20},
     ]
 
     level2 = [
-        {name : "golem", health : 50, strength : 15, defense : 15, dexterity : 15},
-        {name : "troll", health : 75, strength : 20, defense : 20, dexterity : 20},
+        {name: "golem", health: 50, strength: 15, defense: 15, dexterity: 15},
+        {name: "troll", health: 75, strength: 20, defense: 20, dexterity: 20},
     ]
 
-    boss = [{name : "dragon", health : 150, strength : 25, defense : 25, dexterity : 25}]
+    boss = [{name: "dragon", health: 150, strength: 25, defense: 25, dexterity: 25}]
+
 
 def characterStats(character):
     print("You will roll 3 dice to determine your stats.")
-    dexterity = random.randint(4,20)
-    strength = random.randint(4,20)
-    defense = random.randint(4,20)
+    dexterity = random.randint(4, 20)
+    strength = random.randint(4, 20)
+    defense = random.randint(4, 20)
     print("Your dexterity is:", dexterity)
     print("Your strength is:", strength)
     print("Your defense is:", defense)
     reroll = input("Would you like to reroll? ")
     if reroll.lower() == "yes":
-        dexterity = random.randint(1,20)
-        strength = random.randint(1,20)
-        defense = random.randint(1,20)
+        dexterity = random.randint(1, 20)
+        strength = random.randint(1, 20)
+        defense = random.randint(1, 20)
         print("Your dexterity is:", dexterity)
         print("Your strength is:", strength)
         print("Your defense is:", defense)
@@ -63,6 +66,7 @@ def characterStats(character):
     character["strength"] = strength
     character["defense"] = defense
     return character
+
 
 def characterInventory(character):
     print("You have:")
@@ -111,7 +115,7 @@ def characterInventory(character):
             return
         else:
             print("You cannot use that item.")
-        
+
         # Update character document in the database
 
     else:
@@ -122,51 +126,53 @@ def characterSave(character):
     client = get_cosmos_client()
     database = client.get_database_client("Dungeon")
     container = database.get_container_client("Character")
-    container.upsert_item(character)
+    container.upsert_item(body=character)
     print("Character saved")
 
 
 def dungeon(character):
     turns = 0
-    
+
     events = [
         ("You find a treasure chest!", 0.3),
         ("You encounter a monster!", 0.5),
         ("You stumble upon a hidden passage.", 0.2)
     ]
-    
+
     output = ""
-    
+
     while True:
         turns += 1
-        
+
         event = random.choices(events, [weight for _, weight in events])[0]
         output += event[0] + "<br>"
-        
+
         if "treasure chest" in event[0]:
             output += chest() + "<br>"
         elif "monster" in event[0]:
-            output += battle() + "<br>"
+            output += battle(character) + "<br>"
         elif "hidden passage" in event[0]:
             # Generate a random puzzle or secret room
             pass
-        
+
         choice = input("What do you want to do? (continue, inventory, quit) ")
         if choice.lower() != "continue" and choice.lower() != "inventory":
             break
         elif choice.lower() == "inventory":
             output += characterInventory(character) + "<br>"
-    
+
     return output
 
 
-def chest():    
+def chest():
     item = loot_table()
-    
+
     print("You found a", item, "in the chest!")
-    #append item to inventory
+    # append item to inventory
+    characterSave(item)
 
     return item
+
 
 def puzzle():
     print("You come across a door with a strange inscription on it.")
@@ -176,15 +182,15 @@ def puzzle():
         ("gold", 0.3),
         ("scroll", 0.2)
     ]
-    
+
     # Choose a random puzzle
     puzzles = [
         ("What has to be broken before you can use it?", "An egg"),
         ("I am not alive, but I grow; I don't have lungs, but I need air; I don't have a mouth, but water kills me. What am I?", "Fire"),
         ("I have cities, but no houses. I have mountains, but no trees. I have water, but no fish. What am I?", "Map")
     ]
-    puzzle, answer = random.choice(puzzles)
-    
+    puzzles, answer = random.choice(puzzles)
+
     while True:
         guess = input(puzzle + " ")
         if guess.lower() == answer.lower():
@@ -196,34 +202,31 @@ def puzzle():
             print("Incorrect, try again.")
 
 
-def battle():
+def battle(character):
     damage = 0
-    monsters = [
-        ("goblin", 0.5),
-        ("skeleton", 0.3),
-        ("troll", 0.2)
-    ]
-
     monster = random.choices(monsters, [weight for _, weight in monsters])[0]
-    
+
     print("You encounter a", monster[0] + "!")
-    
-    with shelve.open("DungeonCrawl") as db:
-        character = db["character"]
-    
+
+    # get character data from database
+    client = get_cosmos_client()
+    database = client.get_database_client("Dungeon")
+    container = database.get_container_client("Character")
+    character = container.read_item(item=character["id"], partition_key=character["name"])
+
     turn = 1
-    
+
     # Initialize monster health
     monster_health = 100
-    
+
     while True:
         print("\nTurn", turn)
         attack_value = random.randint(1, character["strength"])
         print("Your health:", character["health"])
         print("The", monster[0], "health:", monster_health)
-        
-        choice = input("What do you want to do? (attack, defend, inventory, run) ")
-        
+
+        choice = input("What do you want to do? (attack, defend, heal, run) ")
+
         if choice.lower() == "attack":
             # Calculate damage done to the monster
             damage = attack_value
@@ -232,6 +235,12 @@ def battle():
         elif choice.lower() == "defend":
             defense_value = random.randint(1, character["defense"])
             print("You defend for", defense_value, "damage!")
+        elif choice.lower() == "heal":
+            heal_value = 10
+            character["health"] += heal_value
+            if character["health"] > 100:
+                character["health"] = 100
+            print("You heal for", heal_value, "health!")
         elif choice.lower() == "run":
             run_value = random.randint(1, character["dexterity"])
             if run_value > 10:
@@ -245,17 +254,16 @@ def battle():
         else:
             print("Invalid input")
             continue
-        
+
         monster_attack_value = random.randint(1, 10)
         print("The", monster[0], "attacks for", monster_attack_value, "damage!")
-        
+
         player_damage = monster_attack_value - defense_value if choice.lower() == "defend" else monster_attack_value
-        
+
         character["health"] -= player_damage
         if character["health"] < 0:
             character["health"] = 0
-        
-        
+
         print("You take", player_damage, "damage!")
         print("The", monster[0], "takes", damage, "damage!")
         item = loot_table()
@@ -264,25 +272,25 @@ def battle():
             quit()
         elif monster_health <= 0:
             print("You win!")
-            with shelve.open("DungeonCrawl") as db:
-                if "inventory" not in db:
-                    character["inventory"] = []
-                
-                character["inventory"].append(item)
+            # append item to inventory
+
             print("You found a", item, "on the monster!")
             return
-        
+
         # Increment turn counter
         turn += 1
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.route("/dungeon", methods=["GET", "POST"])
 def dungeon_route():
     character = characterCreate()
     dungeon(character)
+
 
 @app.route("/create", methods=["GET", "POST"])
 def create_character():
@@ -299,14 +307,14 @@ def create_character():
     return render_template("create.html")
 
 
-def characterCreate(name=None, char_class=None):
+def characterCreate(name, char_class):
     character = {
         "name": name,
         "class": char_class,
         "health": 100,
-        "dexterity": 0,
-        "strength": 0,
-        "defense": 0,
+        "strength": 10,
+        "defense": 10,
+        "dexterity": 10,
         "inventory": []
     }
 
@@ -317,7 +325,10 @@ def characterCreate(name=None, char_class=None):
     elif char_class.lower() == "rogue":
         character["dexterity"] += 5
 
-    characterSave(character)
+    client = get_cosmos_client()
+    database = client.get_database_client("Dungeon")
+    container = database.get_container_client("Character")
+    container.upsert_item(character)
 
     return character
 
